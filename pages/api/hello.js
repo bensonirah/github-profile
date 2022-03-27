937617b0dfac220fa240b5ae3e4b1a1d62495609
bensonirah <bensonirah@gmail.com>
2022-03-27 15:16:47 +0300

    add .env files

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from "fs";
 import path from "path";
-import "dotenv/config";
 
-const { IMAGES_PATH } = process.env;
 
 export default function handler(req, res) {
+  const { IMAGES_PATH } = process.env;
   const img = fs.readFileSync(path.join(IMAGES_PATH, "stat.svg"));
   res.setHeader("Content-Type", "image/svg+xml");
   res.send(img);
