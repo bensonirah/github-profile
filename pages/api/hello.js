2f508a265f1ed04f46157c2b89eec92831dbfd7d
bensonirah <bensonirah@gmail.com>
2022-03-27 13:04:42 +0300

    init a github-profile

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fs from "fs";
+import path from "path";
+import "dotenv/config";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const { IMAGES_PATH } = process.env;
+  const img = fs.readFileSync(path.join(IMAGES_PATH, "stat.svg"));
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.send(img);
 }
